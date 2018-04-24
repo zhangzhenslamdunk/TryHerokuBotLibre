85afdfafcb0fd12730cd268cdf243f4cd7510b8d
Zhang Zhen <zhangzhenslamdunk@hotmail.com>
2018-04-24 14:08:12 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# TryHerokuBotLibre
+# Api.ai - sample webhook implementation in Python
+
+This is a really simple webhook implementation that gets Api.ai classification JSON (i.e. a JSON output of Api.ai /query endpoint) and returns a fulfillment response.
+
+More info about Api.ai webhooks could be found here:
+[Api.ai Webhook](https://docs.api.ai/docs/webhook)
+
+# Deploy to:
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+# What does the service do?
+It's a workitem create service to create checkbook 
+The services takes the `workitemtype` parameter from the action, performes create for workitemtype. 
+
+The service packs the result in the Api.ai webhook-compatible response JSON and returns it to Api.ai.
+
---
+++ app.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+
+    speech = 'this is the first message sent from Heroku.'
+    
+#    # read from web data
+#    url = urllib.request.urlopen("http://zhangzhenslamdunk.github.io/sentence.txt") 
+#    s = url.read()
+#    s = s.decode('utf-8')
+#    
+#    # read text file from local folder
+    infile = open('sentence.txt')
+    s = infile.read()
+    
+    speech = s
+    
+    
+    print("Response:")
+    print(speech)
+    return {
+        "fulfillmentText": speech,
+    }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print ("Starting app on port %d" %(port))
+
+    app.run(debug=True, port=port, host='0.0.0.0')
