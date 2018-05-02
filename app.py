e3a3dee61c2de17271d812f8f4daf32041b9b4dd
Zhang Zhen <zhangzhenslamdunk@hotmail.com>
2018-05-02 17:05:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,12 @@ from flask_cors import CORS
 # Flask app should start in global layout
 app = Flask(__name__)
 
-
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 
 
